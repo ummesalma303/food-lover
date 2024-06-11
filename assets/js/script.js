8271e2067075157595bac49729c4d10eaacb8a3e
Umme Salma <salmatahermunni@gmail.com>
2024-06-11 22:25:55 +0600

    added footer

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+const imageArray = [
+    {url:"./assets/images/bibimbap.jpeg"},
+    {url:"./assets/images/corn-dog.jpeg"},
+    {url:"./assets/images/gimbap.jpg"},
+    {url:"./assets/images/kimchi.jpg"},
+    {url:"./assets/images/popular-image-1.png"},
+    {url:"./assets/images/tteokbokki.jpeg"},
+]
+
+function showmoreImage() {
+    const populaContainer2 = document.querySelector(".popular-container-2")
+    const newImage = document.createElement('div')
+    console.log('click',populaContainer2);
+    for (const item of imageArray) {
+        const newImageDiv = document.createElement('div');
+        newImageDiv.innerHTML = `<img src ="${item.url}">`
+        console.log(newImageDiv);
+        populaContainer2.appendChild(newImageDiv)
+        newImage.style.width = '800px';
+    }
+}
+const btn = document.querySelector(".popular-btn");
+btn.innerText = 'see less';
+// form validation 
+function handleContact(event) {
+    event.preventDefault()
+    // console.log('contact');
+    // console.log(event.target);
+    const email = event.target.email.value
+    console.log(email);
+    const element = document.getElementById('element')
+    console.log(element);
+    const newElement = document.createElement('p');
+    newElement.innerText = `Your name: ${email}`;
+    console.log(newElement);
+    element.appendChild(newElement)
+}
